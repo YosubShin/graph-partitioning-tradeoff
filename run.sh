363a320e97d2e42027e34ba97d7817f2e172154c
Yosub Shin <yosub@yosubshin.com>
2015-09-22 00:36:09 -0500

    changed path to jar file

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ export GRAPH_FILE_PATH=hdfs://test-01/user/yosub_shin_0/graph-data/lab_data_lj
 ~/spark-1.5.0-bin-hadoop2.6/bin/spark-submit \
 --class GraphPartitioningTradeoff \
 --master spark://test-01:7077 \
-~/spark-program/target/scala-2.10/graph-partitioning-tradeoff_2.10-0.1-SNAPSHOT.jar
+target/scala-2.10/graph-partitioning-tradeoff_2.10-0.1-SNAPSHOT.jar
