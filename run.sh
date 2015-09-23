f293b6730172c86744484d7138846408dd484f18
Yosub Shin <yosub@yosubshin.com>
2015-09-23 01:39:58 -0500

    Deduce hostname from GCloud API

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 TIME=`date +%Y-%m-%d.%H:%M:%S`
+HOSTNAME=$(curl http://metadata/computeMetadata/v1/instance/hostname -H "Metadata-Flavor: Google")
 export OUTPUT_CSV_PATH=/home/yosub_shin_0/graph-partitioning-tradeoff-${TIME}.csv
-export GRAPH_FILE_PATH=hdfs://test-01/user/yosub_shin_0/graph-data/lab_data_lj
+export GRAPH_FILE_PATH=hdfs://${HOSTNAME}/graph-data/lab_data_lj
 
 ~/spark-1.5.0-bin-hadoop2.6/bin/spark-submit \
 --class GraphPartitioningTradeoff \
---master spark://test-01:7077 \
+--master spark://${HOSTNAME}:7077 \
 target/scala-2.10/graph-partitioning-tradeoff_2.10-0.1-SNAPSHOT.jar
